e66d74e2b39d93911187f3c98bea2c0dee1f6a73
nampham148 <phamquangnam148@gmail.com>
2019-02-16 14:18:00 -0500

    reading api image

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require( 'express-handlebars');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var fileUpload = require('express-fileupload');
+require('dotenv').config()
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -28,7 +33,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.engine( 'hbs', hbs( {
+app.engine('hbs', hbs( {
   extname: 'hbs',
   defaultView: 'default',
   layoutsDir: __dirname + '/views/layouts/',
